cf9163bbe62b8f9ba7b6a422c9965178ee398cb3
Sondre Utheim <sondreutheim@gmail.com>
2023-07-12 12:21:46 +0200

    removed micropython code

--- src/bin/ship.rs
+++ src/bin/ship.rs
@@ -2,7 +2,7 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
-use common::wifi_system::wifi_system;
+use common::{wifi_system::wifi_system, locomotion_system::locomotion_system, thruster::Thruster};
 use defmt::unwrap;
 use embassy_executor::Spawner;
 use {defmt_rtt as _, panic_probe as _};
@@ -13,10 +13,10 @@ async fn main(spawner: Spawner) {
 
     // let send_spawner = spawner.make_send();
 
-    unwrap!(spawner.spawn(wifi_system(
-       spawner, p.PIN_23, p.PIN_25, p.PIO0, p.PIN_24, p.PIN_29, p.DMA_CH0
-    )));
+    // unwrap!(spawner.spawn(wifi_system(
+    //    spawner, p.PIN_23, p.PIN_25, p.PIO0, p.PIN_24, p.PIN_29, p.DMA_CH0
+    // )));
 
-    // let thruster = Thruster::new(p.PIN_4, p.PWM_CH2, p.PIN_2.into(), p.PIN_3.into());
-    // unwrap!(spawner.spawn(locomotion_system(spawner, thruster)));
+    let thruster = Thruster::new(p.PIN_4, p.PWM_CH2, p.PIN_2.into(), p.PIN_3.into());
+    unwrap!(spawner.spawn(locomotion_system(spawner, thruster)));
 }
